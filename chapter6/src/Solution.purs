ee6039adae5b576041892f70e43ab4f693bf128e
shybyte <ein@volloeko.de>
2015-09-04 19:01:16 +0200

    6.4 Write a Semigroup instance for non-empty arrays by reusing the Semigroup instance for Array.

--- chapter6/src/Solution.purs
+++ chapter6/src/Solution.purs
@@ -2,6 +2,7 @@ module Solution where
 
 import Prelude
 import Data.Maybe
+import Data.Array
 
 
 newtype Complex = Complex
@@ -15,3 +16,13 @@ instance showComlex :: Show Complex where
 instance eqComlex :: Eq Complex where
   eq (Complex c1) (Complex c2) = c1.real == c2.real && c1.imaginary == c2.imaginary
 
+
+
+data NonEmpty a = NonEmpty a (Array a)
+
+instance semigroupNonEmpty :: Semigroup (NonEmpty a) where
+  append (NonEmpty a1 array1) (NonEmpty a2 array2 )= NonEmpty a1 ( a2 : (array1 <> array2))
+
+
+--instance showNonempty :: Show (NonEmpty a) where
+--  show (NonEmpty a array) = "Nonempty (" ++ show (a:array)  ++ ")"
