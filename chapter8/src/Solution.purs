838b14a3cba01aabcb40caf4038bb5052f0612f4
shybyte <Marco.Stahl@gmx.de>
2015-09-17 11:43:25 +0200

    8.7.4 Verify that the monad laws hold for the Monad instance for the Maybe type, as defined in the purescript-maybe package.

--- chapter8/src/Solution.purs
+++ chapter8/src/Solution.purs
@@ -47,3 +47,72 @@ sums =
 --  f <- mf -- "early return" if mf is Nothing
 --  a <- ma -- "early return" if ma is Nothing
 --  return (f a) -- f <$> ma == map f ma
+
+
+-- 8.7.3 Verify that the monad laws hold for the Monad instance for the Maybe type, as defined in the purescript-maybe package.
+
+
+-- class (Functor f) <= Apply f where
+--   apply :: forall a b. f (a -> b) -> f a -> f b
+--
+-- class (Apply f) <= Applicative f where
+--   pure :: forall a. a -> f a
+--
+-- class (Apply m) <= Bind m where
+--   bind :: forall a b. m a -> (a -> m b) -> m b
+--
+-- class (Applicative m, Bind m) <= Monad m
+--
+-- instance applicativeMaybe :: Applicative Maybe where
+--   pure = Just
+--
+-- instance bindMaybe :: Bind Maybe where
+--   bind (Just x) k = k x
+--   bind Nothing  _ = Nothing
+--
+-- -- Right identity law
+--
+-- rightIdentity monad = do -- == monad
+--   x <- monad
+--   return x
+--
+-- rightIdentity2 Nothing = Nothing
+-- rightIdentity2 Just a = Just a
+--
+-- -- Left identity law
+--
+-- leftIdentity next = do  -- == next
+--   x <- return y
+--   next
+--
+-- leftIdentity2 next = bind (Just a) (\x -> next) -- = next
+--
+--
+-- -- Associativity law.
+--
+-- c1 = do
+--   y <- do
+--     x <- m1
+--     m2
+--   m3
+-- c1' = bind (bind m1 m2) m3
+--
+-- c2 = do
+--   x <- m1
+--   y <- m2
+--   m3
+-- c2' = bind m1 (bind m2 m3)
+--
+-- -- c1 == c2
+--
+-- c3 = do
+--   x <- m1
+--   do
+--     y <- m2
+--     m3
+--
+-- c3' = bind m1 (bind m2  m3)
+--
+-- -- c1 == c2 == c3
+
+
