988d7c9ee3954c0e089d20c4d6de8dcdb478e7b2
shybyte <ein@volloeko.de>
2016-05-25 08:46:56 +0200

    Exercise 3.15 3) findEntryByAddress

--- chapter3/src/Data/AddressBook.purs
+++ chapter3/src/Data/AddressBook.purs
@@ -1,15 +1,14 @@
 module Data.AddressBook where
 
-import Prelude
-
-import Data.List
-import Data.Maybe
+import Prelude ((<<<), (==), (&&), (++))
+import Data.List (List(Cons), filter, head)
+import Data.Maybe (Maybe)
 
 import Control.Plus (empty)
 
-type Address = 
+type Address =
   { street :: String
-  , city   :: String 
+  , city   :: String
   , state  :: String
   }
 
@@ -32,9 +31,19 @@ emptyBook = empty
 
 insertEntry :: Entry -> AddressBook -> AddressBook
 insertEntry = Cons
- 
+
 findEntry :: String -> String -> AddressBook -> Maybe Entry
-findEntry firstName lastName = head <<< filter filterEntry 
+findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+
+findEntryByAddress :: Address -> AddressBook -> Maybe Entry
+findEntryByAddress address = head <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry =
+    entry.address.street == address.street &&
+    entry.address.city == address.city &&
+    entry.address.state == address.state
--- chapter3/test/Main.purs
+++ chapter3/test/Main.purs
@@ -1,15 +1,14 @@
 module Test.Main where
 
-import Prelude
+import Prelude (Unit, (<$>), ($), bind)
 
 import Control.Monad.Eff
-import Control.Monad.Eff.Console
-
-import Data.Maybe
-import Data.AddressBook
+import Control.Monad.Eff.Console (CONSOLE, print)
+import Data.Maybe (Maybe)
+import Data.AddressBook (Address, AddressBook, Entry, findEntryByAddress, showEntry, emptyBook, insertEntry, findEntry)
 
 example :: Entry
-example = 
+example =
   { firstName: "John"
   , lastName: "Smith"
   , address: { street: "123 Fake St."
@@ -17,15 +16,26 @@ example =
              , state: "CA"
              }
   }
-     
+
 book0 :: AddressBook
 book0 = emptyBook
 
 printEntry :: String -> String -> AddressBook -> Maybe String
 printEntry firstName lastName book = showEntry <$> findEntry firstName lastName book
 
+unknownAddress :: Address
+unknownAddress =
+  { street: "Ourstreet 12"
+  , city: "Berlin"
+  , state: "Germany"
+  }
+
+main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = do
   let book1 = insertEntry example emptyBook
 
   print $ printEntry "John" "Smith" book0
   print $ printEntry "John" "Smith" book1
+
+  print $ showEntry <$> findEntryByAddress example.address book1
+  print $ showEntry <$> findEntryByAddress unknownAddress book1
