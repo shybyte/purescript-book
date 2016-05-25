85ee4bdc329ca1a38c3740d30492f1193cab5cf9
shybyte <ein@volloeko.de>
2016-05-25 07:26:10 +0200

    Exercises 2.11 circleArea and purescript-globals

--- chapter2/src/Main.purs
+++ chapter2/src/Main.purs
@@ -1,10 +1,19 @@
 module Main where
 
-import Prelude ((+), (*))
-import Math (sqrt)
-import Control.Monad.Eff.Console (print)
+import Control.Monad.Eff
+import Prelude ((+), (*), Unit)
+import Math (sqrt,pi)
+import Control.Monad.Eff.Console (CONSOLE, print)
+import Global (readFloat)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea :: Number -> Number
+circleArea r = r * r * pi
+
+circleAreaFromString :: String -> Number
+circleAreaFromString rString = circleArea (readFloat rString)
+
+main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = print (diagonal 3.0 4.0)
