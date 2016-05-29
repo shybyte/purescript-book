c2645f1e539e41e1017b268c367dc8c08c845819
shybyte <ein@volloeko.de>
2016-05-29 11:52:14 +0200

    5.13 Exercises

--- chapter5/src/Data/Picture.purs
+++ chapter5/src/Data/Picture.purs
@@ -1,32 +1,32 @@
 module Data.Picture where
 
-import Prelude
+import Data.Foldable (foldl)
+import Data.Maybe (Maybe(Nothing, Just))
+import Prelude ((*), negate, (/), (+), (-), show, (++), map)
 
-import Data.Foldable
-
-data Point = Point 
+data Point = Point
   { x :: Number
-  , y :: Number 
+  , y :: Number
   }
 
 showPoint :: Point -> String
-showPoint (Point { x = x, y = y }) = 
+showPoint (Point { x = x, y = y }) =
   "(" ++ show x ++ ", " ++ show y ++ ")"
 
-data Shape 
+data Shape
   = Circle Point Number
   | Rectangle Point Number Number
   | Line Point Point
   | Text Point String
 
 showShape :: Shape -> String
-showShape (Circle c r) = 
+showShape (Circle c r) =
   "Circle [center: " ++ showPoint c ++ ", radius: " ++ show r ++ "]"
-showShape (Rectangle c w h) = 
+showShape (Rectangle c w h) =
   "Rectangle [center: " ++ showPoint c ++ ", width: " ++ show w ++ ", height: " ++ show h ++ "]"
-showShape (Line start end) = 
+showShape (Line start end) =
   "Line [start: " ++ showPoint start ++ ", end: " ++ showPoint end ++ "]"
-showShape (Text loc text) = 
+showShape (Text loc text) =
   "Text [location: " ++ showPoint loc ++ ", text: " ++ show text ++ "]"
 
 type Picture = Array Shape
@@ -38,11 +38,11 @@ data Bounds = Bounds
   { top    :: Number
   , left   :: Number
   , bottom :: Number
-  , right  :: Number 
+  , right  :: Number
   }
 
 showBounds :: Bounds -> String
-showBounds (Bounds b) = 
+showBounds (Bounds b) =
   "Bounds [top: " ++ show b.top ++
   ", left: "      ++ show b.left ++
   ", bottom: "    ++ show b.bottom ++
@@ -75,21 +75,25 @@ shapeBounds (Text (Point { x = x, y = y }) _) = Bounds
   , right:  x
   }
 
-(\/) :: Bounds -> Bounds -> Bounds
-(\/) (Bounds b1) (Bounds b2) = Bounds
+intersection :: Bounds -> Bounds -> Bounds
+intersection (Bounds b1) (Bounds b2) = Bounds
   { top:    Math.min b1.top    b2.top
   , left:   Math.min b1.left   b2.left
   , bottom: Math.max b1.bottom b2.bottom
   , right:  Math.max b1.right  b2.right
   }
 
-(/\) :: Bounds -> Bounds -> Bounds
-(/\) (Bounds b1) (Bounds b2) = Bounds
+infix 4 intersection as \/
+
+union :: Bounds -> Bounds -> Bounds
+union (Bounds b1) (Bounds b2) = Bounds
   { top:    Math.max b1.top    b2.top
   , left:   Math.max b1.left   b2.left
   , bottom: Math.min b1.bottom b2.bottom
   , right:  Math.min b1.right  b2.right
   }
+
+infix 4 union as /\
 
 emptyBounds :: Bounds
 emptyBounds = Bounds
@@ -112,3 +116,40 @@ bounds = foldl combine emptyBounds
   where
   combine :: Bounds -> Shape -> Bounds
   combine b shape = shapeBounds shape \/ b
+
+
+-- 1. Exercise
+-- Construct a value of type Shape which represents a circle centered at the origin with radius 10.0.
+
+centeredCircle :: Shape
+centeredCircle = Circle (Point {x: 0.0, y: 0.0}) 10.0
+
+
+-- 2. Exercise
+-- Write a function from Shapes to Shapes, which scales its argument by a factor of 2.0,
+-- center the origin.
+
+scale ::  Shape -> Shape
+scale (Circle center r) = Circle center (2.0 * r)
+scale (Rectangle center w h) = Rectangle center (2.0 * w) (2.0 * h)
+scale (Line (Point p1) (Point p2)) = Line (Point p1') (Point p2')
+  where
+  centerX = mean p1.x p2.x
+  centerY = mean p1.y p2.y
+  p1' = { x: 2.0 * (p1.x - centerX) + centerX,  y: 2.0 * (p1.y - centerY) + centerY}
+  p2' = { x: 2.0 * (p2.x - centerX) + centerX,  y: 2.0 * (p2.y - centerY) + centerY}
+scale text = text
+
+
+mean :: Number -> Number -> Number
+mean x y = (x + y) / 2.0
+
+
+-- 3. Exercise
+-- Write a function which extracts the text from a Shape.
+-- It should return Maybe String, and use the Nothing constructor
+-- if the input is not constructed using Text.
+
+extractText :: Shape -> Maybe String
+extractText (Text _ text) = Just text
+extractText _ = Nothing
