3a966db60198b9f2b90607d2443f3870ed68c049
shybyte <ein@volloeko.de>
2016-06-18 11:03:22 +0200

    9.8 Global Mutable State - 1. Exercise fillStrokePath

--- chapter9/src/Example/Random.purs
+++ chapter9/src/Example/Random.purs
@@ -11,27 +11,33 @@ import Control.Monad.Eff.Random
 
 import Graphics.Canvas
 
+fillStrokePath :: forall eff a. Context2D -> Eff (canvas :: Canvas | eff) a -> Eff (canvas :: Canvas | eff) a
+
+fillStrokePath ctx path = do
+  fillPath ctx path
+  strokePath ctx path
+
 main = do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
 
   setFillStyle "#FF0000" ctx
-  setStrokeStyle "#000000" ctx 
+  setStrokeStyle "#000000" ctx
 
   for_ (1 .. 100) \_ -> do
     x <- random
     y <- random
     r <- random
 
-    let path = arc ctx 
+    let path = arc ctx
          { x     : x * 600.0
          , y     : y * 600.0
          , r     : r * 50.0
          , start : 0.0
          , end   : Math.pi * 2.0
          }
-    
-    fillPath ctx path
-    strokePath ctx path
+
+
+    fillStrokePath ctx path
 
     return unit
