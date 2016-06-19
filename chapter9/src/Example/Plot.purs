26657aee8702409064facb1e978294e247d06188
shybyte <ein@volloeko.de>
2016-06-19 07:48:53 +0200

    9.8 Global Mutable State - 3.Exercise (rotateAroundPoint)

--- chapter9/src/Example/Plot.purs
+++ chapter9/src/Example/Plot.purs
@@ -1,6 +1,6 @@
 module Example.Plot where
 
-import Prelude ((*), (+), bind, (/), (>>>), (<$>), unit, return, ($))
+import Prelude ((*), (+), (-), bind, (/), (>>>), (<$>), unit, return, ($))
 import Data.Int (toNumber)
 import Data.Array ((..))
 import Data.Maybe.Unsafe (fromJust)
@@ -26,12 +26,20 @@ plot ctx f = do
   let points = (toNumber >>> (\x -> x / 100.0) >>> f) <$> (0 .. 100)
   renderPath ctx points
 
+rotateAroundPoint :: forall eff. Context2D -> Number -> Point -> Eff (canvas :: Canvas | eff) Context2D
+rotateAroundPoint ctx a center = do
+  translate { translateX: center.x, translateY:  center.y } ctx
+  rotate a ctx
+  translate { translateX: 0.0-center.x, translateY:  0.0 - center.y } ctx
+
 
 main :: forall eff. Eff (canvas:: Canvas | eff) Context2D
 main = do
   canvasMaybe <- getCanvasElementById "canvas"
   let canvas = fromJust canvasMaybe
   ctx <- getContext2D canvas
+
+  rotateAroundPoint ctx 0.8 {x: 300.0, y: 300.0}
 
   setStrokeStyle "#FF0000" ctx
   renderPath ctx  [{x: 10.0,y: 10.0}, {x: 50.0,y: 50.0}, {x: 100.0,y: 150.0}]
